0bfaa970114694819f231ef3bade441fbfd13f6c
Jorge Hernandes <jorgeh@ciandt.com>
2017-07-20 18:18:29 -0300

    Refactor e tratamento assícrono com async/await

--- app.js
+++ app.js
@@ -1,33 +1,34 @@
 const { promisify } = require('util');
 
-var gapi = require('googleapis');
-var gsheets = gapi.sheets('v4');
-var key = require('./keys/LogWork-04cbe7313912.json');
+const gapi = require('googleapis');
+const gsheets = gapi.sheets('v4');
+const key = require('./keys/LogWork-04cbe7313912.json');
 
-sheetsGet = promisify(gsheets.spreadsheets.values.get);
-
-var jwtClient = new gapi.auth.JWT(
+const jwtClient = new gapi.auth.JWT(
   key.client_email,
   null,
   key.private_key,
   ['https://www.googleapis.com/auth/spreadsheets'], // an array of auth scopes 
   null
 );
- 
-jwtClient.authorize(function (err, tokens) {
-  if (err) {
-    console.log(err);
-    return;
-  }
- 
-  sheetsGet({
-    spreadsheetId: '1y9pRKiqncE9mXjsJqZR_VUyB4EH1_KC5BnzpNYluDzY',
-    range: 'FT2017!A2:G1000',
-    auth: jwtClient
-  }).then((response) => {
-    console.log(response)
-  }).catch((error) => {
-    console.log(error)
-  });
 
-});
+const authorize = promisify(jwtClient.authorize);
+const sheetsGet = promisify(gsheets.spreadsheets.values.get);
+
+(async function() {
+  try {
+
+    let linhas = await sheetsGet({
+      spreadsheetId: '1y9pRKiqncE9mXjsJqZR_VUyB4EH1_KC5BnzpNYluDzY',
+      range: 'FT2017!A2:G1000',
+      auth: jwtClient
+    });
+        
+    console.log(linhas);
+
+  } catch (e) {
+
+    console.log(e);
+    
+  }
+})();
